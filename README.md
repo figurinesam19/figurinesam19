470e14775541537e88ad58b951f70c079ab43a10
Samuel <l-adresse-noreply-affiche>
2026-09-25 11:27:14 +0200

    Portrait recentré sur le visage

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 <div align="center">
 
 <h3><code>sam@github ~ $ whoami</code></h3>
-<img src="./portrait-v3.svg" width="650" />
+<img src="./portrait-v4.svg" width="650" />
 
 </div>
--- scripts/square_crop.py
+++ scripts/square_crop.py
@@ -1,10 +1,14 @@
+import sys
 from PIL import Image, ImageOps
+KEEP = float(sys.argv[1]) if len(sys.argv) > 1 else 1.0
 im = Image.open("source-prepped.png").convert("L")
 mask = ImageOps.invert(im).point(lambda p: 255 if p > 10 else 0)
 im = im.crop(mask.getbbox())
 w, h = im.size
+im = im.crop((0, 0, w, int(h * KEEP)))
+w, h = im.size
 s = int(max(w, h) * 1.05)
 canvas = Image.new("L", (s, s), 255)
-canvas.paste(im, ((s - w) // 2, (s - h) // 2))
-canvas.save("source-prepped.png")
-print("recadré :", w, "x", h, "->", s, "x", s)
+canvas.paste(im, ((s - w) // 2, s - h))
+canvas.save("source-square.png")
+print("garde", int(KEEP * 100), "% du sujet ->", s, "x", s)
